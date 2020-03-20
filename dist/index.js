63dbf46adf5f0b82a88769087401806aded599d7
Lucas Pinheiro <lucaspinheiroifsul@gmail.com>
2020-03-20 20:25:21 -0300

    compile with ncc

--- dist/index.js
+++ dist/index.js
@@ -56,14 +56,20 @@ module.exports = require("os");
 const core = __webpack_require__(703)
 
 try {
-  const stringArray = core.getInput("stringArray")
-  if (!stringArray) throw new Error("stringArray's required.")
+  const stringArrayInput = core.getInput("stringArray")
+  if (!stringArrayInput) throw new Error("stringArray's required.")
 
-  console.log("---INPUTS---")
-  console.log("stringArray: ", stringArray)
+  let stringArray = stringArrayInput
+  const isArray = Array.isArray(stringArrayInput)
 
-  const isArray = Array.isArray(stringArray)
-  if (!isArray) throw new Error("stringArray must be an Array.")
+  if (!isArray) {
+    stringArray = stringArrayInput
+      .substring(1, stringArrayInput.length - 1)
+      .split(",")
+  }
+
+  if (!Array.isArray(stringArray))
+    throw new Error("stringArray must be an Array.")
 
   const allElementsAreString = stringArray.every(el => typeof el === "string")
   if (!allElementsAreString)
@@ -71,6 +77,8 @@ try {
 
   const stringList = "- " + stringArray.join("\n- ")
 
+  console.log("---INPUTS---")
+  console.log("stringArray: ", stringArray)
   console.log("\n---OUTPUTS---")
   console.log("stringList:")
   console.log(stringList)
