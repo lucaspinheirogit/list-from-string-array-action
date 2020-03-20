643a76a3eda10932db9f728a7abc6b718edd5ca9
Lucas Pinheiro <lucaspinheiroifsul@gmail.com>
2020-03-20 20:03:12 -0300

    bugfixes

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const core = require("@actions/core")
 
 try {
-  const stringArray = core.getInput("stringArray")
-  if (!stringArray) throw new Error("stringArray's required.")
+  const stringArrayInput = core.getInput("stringArray")
+  if (!stringArrayInput) throw new Error("stringArray's required.")
 
-  console.log("---INPUTS---")
-  console.log("stringArray: ", stringArray)
+  let stringArray = stringArrayInput
+  const isArray = Array.isArray(stringArrayInput)
 
-  const isArray = Array.isArray(stringArray)
-  if (!isArray) throw new Error("stringArray must be an Array.")
+  if (!isArray) {
+    stringArray = stringArrayInput
+      .substring(1, stringArrayInput.length - 1)
+      .split(",")
+  }
+
+  if (!Array.isArray(stringArray))
+    throw new Error("stringArray must be an Array.")
 
   const allElementsAreString = stringArray.every(el => typeof el === "string")
   if (!allElementsAreString)
@@ -16,6 +22,8 @@ try {
 
   const stringList = "- " + stringArray.join("\n- ")
 
+  console.log("---INPUTS---")
+  console.log("stringArray: ", stringArray)
   console.log("\n---OUTPUTS---")
   console.log("stringList:")
   console.log(stringList)
